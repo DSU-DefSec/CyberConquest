3ddcae1c20f7d8ee97dbcee48ffee00e0bb57202
Goofables <Goofables@gmail.com>
2023-03-30 16:17:02 -0500

    Fixed scoring

--- water-tower-scoring/scoring-server.py
+++ water-tower-scoring/scoring-server.py
@@ -3,7 +3,7 @@
 # @Project: CyberConquest/water-tower-scoring
 # @Author: Goofables
 # @Created: 3/6/23
-
+import dataclasses
 import json
 import time
 from enum import Enum
@@ -14,56 +14,63 @@ import busio
 import digitalio
 from adafruit_mcp230xx.mcp23008 import MCP23008
 from flask import Flask, render_template
-from flask_sock import Sock
+
+MCP_COUNT = 6
+MCP_BASE_ID = 0x20
 
 app = Flask(__name__, template_folder="templates")
 app.config["SOCK_SERVER_OPTIONS"] = {"ping_interval": 25}
-sock = Sock(app)
+
 client_list = []
 
-TEAM_COUNT = 0
+i2c = busio.I2C(board.SCL, board.SDA)
+raw_pins: list = []
+mcp_list: list = [MCP23008(i2c, address=MCP_BASE_ID + i) for i in range(MCP_COUNT)]
+for m in mcp_list:
+    for i in range(8):
+        raw_pins.append(m.get_pin(i))
 
 
-class GPIOController:
-    class State(Enum):
-        EMPTY = 0
-        SCORING = 1
-        OVERFLOW = 2
-        BROKEN = 3
+def pin(mcp_id: int, pin_id: int) -> digitalio.DigitalInOut:
+    p = mcp_list[mcp_id].get_pin(pin_id)
+    p.direction = digitalio.Direction.INPUT
+    return p
 
-    def __init__(self, team_count: int):
-        self.team_count = team_count
 
-        self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.mcp_list: list[MCP23008] = []
-        self.raw_pins: list[digitalio.DigitalInOut] = []
+@dataclasses.dataclass
+class Team:
+    scoring: digitalio.DigitalInOut
+    overflow: digitalio.DigitalInOut
 
-        mcp_id = 0x20
-        while len(self.raw_pins) < team_count:
-            mcp: MCP23008 = MCP23008(self.i2c, address=mcp_id)
-            self.mcp_list.append(mcp)
-            for i in range(8):
-                self.raw_pins.append(mcp.get_pin(i))
 
-        self.overflow_sensors: list[digitalio.DigitalInOut] = []
-        self.scoring_sensors: list[digitalio.DigitalInOut] = []
+teams = [
+    Team(pin(0, 6), pin(0, 7)),
+    Team(pin(5, 4), pin(5, 1)),
+    Team(pin(4, 0), pin(4, 1)),
+    Team(pin(3, 1), pin(3, 0)),
+    Team(pin(2, 1), pin(2, 0)),
 
-        for t in range(team_count):
-            scoring_pin = self.raw_pins[t * 2]
-            scoring_pin.direction = digitalio.Direction.INPUT
-            self.scoring_sensors.append(scoring_pin)
+    Team(pin(1, 1), pin(1, 0)),
+    Team(pin(0, 3), pin(0, 2)),
+    Team(pin(0, 1), pin(0, 0)),#7
+]
 
-            overflow_pin = self.raw_pins[t * 2 + 1]
-            overflow_pin.direction = digitalio.Direction.INPUT
-            self.overflow_sensors.append(overflow_pin)
 
-    def get_team(self, team_id: int) -> str:
-        value = (self.overflow_sensors[team_id].value << 1 & 2) + (self.scoring_sensors[team_id].value & 1)
-        return GPIOController.State(value).name
+class State(Enum):
+    EMPTY = 0
+    SCORING = 1
+    OVERFLOW = 2
+    BROKEN = 3
 
-    def get_teams(self) -> list:
-        # return [self.get_team(t) for t in range(self.team_count)]
-        return [GPIOController.State.OVERFLOW.name]
+
+def get_team(team_id: int) -> str:
+    value = (teams[team_id].overflow.value << 1 & 2) + (teams[team_id].scoring.value & 1)
+    return State(value).name
+
+
+def get_teams() -> list:
+    return [get_team(t) for t in range(len(teams))]
+    # return [State.OVERFLOW.name]
 
 
 def send_update():
@@ -85,30 +92,22 @@ def send_update_loop():
 
 @app.route("/")
 def hello_world():
-    return render_template(
-        "index.html", currentInstruction=json.dumps(app.gpio_controller.get_teams())
-    )
+    return render_template("index.html", currentInstruction=json.dumps(get_teams()))
 
 
 @app.route("/scoring")
 def scoring():
-    return app.gpio_controller.get_teams()
+    return get_teams()
 
 
-@sock.route("/sock")
-def echo(ws):
-    client_list.append(ws)
-    while True:
-        data = ws.receive()
-        if data == "close":
-            break
-    client_list.remove(ws)
+@app.route("/team://<team_ip>/")
+def scoring2(team_ip):
+    team_num = int(team_ip.split(".")[2])
+    return get_team(team_num)
 
 
 if __name__ == "__main__":
-    app.gpio_controller = GPIOController(TEAM_COUNT)
-
     app.send_update_loop_thread = Thread(target=send_update_loop, daemon=True)
     app.send_update_loop_thread.start()
 
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host='0.0.0.0', debug=False, port=80)
