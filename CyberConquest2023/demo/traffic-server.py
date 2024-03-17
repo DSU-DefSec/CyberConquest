7fd27d831c07c9fc8d3bff9f6c7bf7218cd12829
Goofables <Goofables@gmail.com>
2024-03-17 15:16:42 -0500

    Moved to CyberConquest2023 folder

--- CyberConquest2023/demo/traffic-server.py
+++ CyberConquest2023/demo/traffic-server.py
@@ -2,7 +2,7 @@
 # @Name: traffic-server.py
 # @Project: CyberConquest/traffic-light
 # @Author: Doodleman360
-# @Created: 2/22/23
+# @Created: 2023-02-22
 # @Edited by Goofables to be 20% more blinky
 import dataclasses
 import datetime
@@ -31,12 +31,11 @@ global_pixel_lock = threading.Lock()
 INITIAL_CONTROL_CODE = [
     [
         "set:0,255,0,0",
-        "wait:50",
+        "wait:75",
         "set:0,0,0,0",
         "wait:25",
         "wait:0",
         "wait:10",
-
         "wait:0",
         "wait:50",
         "wait:0",
@@ -46,12 +45,11 @@ INITIAL_CONTROL_CODE = [
     ],
     [
         "wait:0",
-        "wait:50",
+        "wait:75",
         "set:255,255,0,0",
         "wait:25",
         "set:0,0,0,0",
         "wait:10",
-
         "wait:0",
         "wait:50",
         "wait:0",
@@ -61,12 +59,11 @@ INITIAL_CONTROL_CODE = [
     ],
     [
         "set:0,0,0,0",
-        "wait:50",
+        "wait:75",
         "wait:0",
         "wait:25",
         "set:255,0,0,0",
         "wait:10",
-
         "wait:0",
         "wait:50",
         "wait:0",
@@ -74,8 +71,7 @@ INITIAL_CONTROL_CODE = [
         "wait:0",
         "wait:10",
     ],
-    [
-        # "set:0,0,0,0",
+    [  # "set:0,0,0,0",
         # "wait:1",
         # "set:255,0,0,0",
         # "wait:1",
@@ -108,19 +104,17 @@ INITIAL_CONTROL_CODE = [
         # "set:255,255,255,255",
         # "wait:1",
     ],
-    [
-        # "shell:curl neverssl.com -s >>/dev/null", # debug testing ONLY
+    [  # "shell:curl neverssl.com -s >>/dev/null", # debug testing ONLY
         # "random:",
         # "wait:50"
     ],
     [
         "set:0,255,0,0",
-        "wait:50",
+        "wait:75",
         "set:0,0,0,0",
         "wait:25",
         "wait:0",
         "wait:10",
-
         "wait:0",
         "wait:50",
         "wait:0",
@@ -130,12 +124,11 @@ INITIAL_CONTROL_CODE = [
     ],
     [
         "wait:0",
-        "wait:50",
+        "wait:75",
         "set:255,255,0,0",
         "wait:25",
         "set:0,0,0,0",
         "wait:10",
-
         "wait:0",
         "wait:50",
         "wait:0",
@@ -145,28 +138,25 @@ INITIAL_CONTROL_CODE = [
     ],
     [
         "set:0,0,0,0",
-        "wait:50",
+        "wait:75",
         "wait:0",
         "wait:25",
         "set:255,0,0,0",
         "wait:10",
-
         "wait:0",
         "wait:50",
         "wait:0",
         "wait:25",
         "wait:0",
         "wait:10",
-    ],
-    # side2
+    ],  # side2
     [
         "wait:0",
-        "wait:50",
+        "wait:75",
         "wait:0",
         "wait:25",
         "wait:0",
         "wait:10",
-
         "set:0,0,0,0",
         "wait:50",
         "wait:0",
@@ -174,15 +164,13 @@ INITIAL_CONTROL_CODE = [
         "set:255,0,0,0",
         "wait:10",
     ],
-
     [
         "wait:0",
-        "wait:50",
+        "wait:75",
         "wait:0",
         "wait:25",
         "wait:0",
         "wait:10",
-
         "wait:0",
         "wait:50",
         "set:255,255,0,0",
@@ -192,12 +180,11 @@ INITIAL_CONTROL_CODE = [
     ],
     [
         "wait:0",
-        "wait:50",
+        "wait:75",
         "wait:0",
         "wait:25",
         "wait:0",
         "wait:10",
-
         "set:0,255,0,0",
         "wait:50",
         "set:0,0,0,0",
@@ -209,12 +196,11 @@ INITIAL_CONTROL_CODE = [
     [],
     [
         "wait:0",
-        "wait:50",
+        "wait:75",
         "wait:0",
         "wait:25",
         "wait:0",
         "wait:10",
-
         "set:0,0,0,0",
         "wait:50",
         "wait:0",
@@ -222,15 +208,13 @@ INITIAL_CONTROL_CODE = [
         "set:255,0,0,0",
         "wait:10",
     ],
-
     [
         "wait:0",
-        "wait:50",
+        "wait:75",
         "wait:0",
         "wait:25",
         "wait:0",
         "wait:10",
-
         "wait:0",
         "wait:50",
         "set:255,255,0,0",
@@ -240,12 +224,11 @@ INITIAL_CONTROL_CODE = [
     ],
     [
         "wait:0",
-        "wait:50",
+        "wait:75",
         "wait:0",
         "wait:25",
         "wait:0",
         "wait:10",
-
         "set:0,255,0,0",
         "wait:50",
         "set:0,0,0,0",
@@ -257,7 +240,6 @@ INITIAL_CONTROL_CODE = [
 
 
 class LightController:
-
     def __init__(self, pixel_count: int):
         self.pixels = neopixel.NeoPixel(
             board.D10,  # D1
@@ -265,7 +247,7 @@ class LightController:
             bpp=4,
             pixel_order=neopixel.GRBW,
             auto_write=False,
-            brightness=0.1
+            brightness=0.1,
         )
         self.light_instruction_ptr: list = []
         self.light_instruction_time: list = []
@@ -283,8 +265,10 @@ class LightController:
 
             change = False
             for i in range(len(self.pixels)):
-                if len(self.light_instructions) <= i: continue
-                if len(self.light_instructions[i]) == 0: continue
+                if len(self.light_instructions) <= i:
+                    continue
+                if len(self.light_instructions[i]) == 0:
+                    continue
                 try:
                     instruction, arg = self.light_instructions[i][self.light_instruction_ptr[i]].split(":")
                     if instruction == "set":
@@ -295,7 +279,7 @@ class LightController:
                             random.randint(0, 255),
                             random.randint(0, 255),
                             random.randint(0, 255),
-                            random.randint(0, 255) if arg == "w" else 0
+                            random.randint(0, 255) if arg == "w" else 0,
                         ]
                         change = True
                     elif instruction == "wait":
@@ -345,10 +329,14 @@ class LightController:
         Wtf even is thread safety??
         """
         val = self.pixels[pixel_id]
-        if red is None: red = val[0]
-        if green is None: green = val[1]
-        if blue is None: blue = val[2]
-        if white is None: white = val[3]
+        if red is None:
+            red = val[0]
+        if green is None:
+            green = val[1]
+        if blue is None:
+            blue = val[2]
+        if white is None:
+            white = val[3]
         self.pixels[pixel_id] = (red, green, blue, white)
 
         self.sync_updates(False)
@@ -382,7 +370,6 @@ class Car:
 
 
 class Cars:
-
     def __init__(self, pixel_count: int):
         self.pixels = neopixel.NeoPixel(
             board.D21,  # D1
@@ -390,12 +377,13 @@ class Cars:
             bpp=3,
             pixel_order=neopixel.GRB,
             auto_write=False,
-            brightness=0.1
+            brightness=0.1,
         )
         self.speed_limit = 0.1
         self.cars: list = []
         self.active = True
         self.total_spawned = 0
+        self.loop_thread = None
 
     def car_driving_loop(self):
         while self.active:
@@ -406,7 +394,7 @@ class Cars:
                         0.1,
                         0.01,
                         (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
-                        random.randint(0, 3)
+                        random.randint(0, 3),
                     )
                 )
                 self.total_spawned += 1
@@ -451,7 +439,7 @@ class Cars:
                     else:
                         c.accel = 0.03
 
-                    c.accel *= 0.9 ** c.length
+                    c.accel *= 0.9**c.length
 
                 c.position += c.velocity
                 c.velocity = min(max(c.velocity + c.accel, 0), 1)
@@ -512,7 +500,7 @@ def send_update_loop():
         if c % 600 == 0:
             with open(f"mem.{datetime.datetime.now().timestamp()}.dump", "w") as f:
                 snapshot = tracemalloc.take_snapshot()
-                f.writelines([a.__str__() + "\n" for a in snapshot.statistics('lineno')[:10]])
+                f.writelines([a.__str__() + "\n" for a in snapshot.statistics("lineno")[:10]])
         c += 1
 
 
@@ -524,27 +512,24 @@ def parse_data(data: dict):
     if "id" in data:
         app.light_controller.set_pixel(
             pixel_id=int(data["id"]),
-            color=(int(data["r"]), int(data["g"]), int(data["b"]), int(data["w"]))
+            color=(int(data["r"]), int(data["g"]), int(data["b"]), int(data["w"])),
         )
     return True
 
 
 @app.route("/")
 def hello_world():
-    return render_template(
-        "index.html", currentInstruction=json.dumps(app.light_controller.light_instructions)
-    )
+    return render_template("index.html", currentInstruction=json.dumps(app.light_controller.light_instructions))
 
 
 @app.route("/stats")
 def stats():
     snapshot = tracemalloc.take_snapshot()
-    return "\n".join([a.__str__() for a in snapshot.statistics('lineno')[:10]])
+    return "\n".join([a.__str__() for a in snapshot.statistics("lineno")[:10]])
 
 
 @app.route("/", methods=["POST"])
 def post_data():
-    # print(request.data)
     return "OK" if parse_data(request.json) else "BAD"
 
 
@@ -555,9 +540,7 @@ def echo(ws):
         data = ws.receive()
         if data == "close":
             break
-
         parse_data(json.loads(data))
-
     client_list.remove(ws)
 
 
@@ -573,4 +556,4 @@ if __name__ == "__main__":
 
     app.send_update_loop_thread.start()
 
-    app.run(host='0.0.0.0', debug=False)
+    app.run(host="0.0.0.0", debug=False)
--- CyberConquest2023/examples/simple_mcp2221.py
+++ CyberConquest2023/examples/simple_mcp2221.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# @Name: simple_test.py
+# @Name: simple_mcp2221.py
 # @Project: CyberConquest/examples
 # @Author: Goofables
 # @Created: 2023-11-29
@@ -7,21 +7,21 @@
 import os
 
 # Set board model
-os.environ["BLINKA_FT232H"] = "1"
+os.environ["BLINKA_MCP2221"] = "1"
 
 import time
 
 import board
 import digitalio
 
-pin = digitalio.DigitalInOut(board.C7)
+pin = digitalio.DigitalInOut(board.G0)
 pin.direction = digitalio.Direction.OUTPUT
 
 while True:
     print("Off")
     pin.value = False
-    time.sleep(3)
+    time.sleep(0.1)
 
     print("On")
     pin.value = True
-    time.sleep(3)
+    time.sleep(0.1)
