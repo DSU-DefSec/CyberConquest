033e77bd523689ed98d2371db328c3820e9d07be
Goofables <Goofables@gmail.com>
2024-04-20 20:10:20 -0400

    Added clock tower

--- CyberConquest2024/demo/clocktower.py
+++ CyberConquest2024/demo/clocktower.py
@@ -3,47 +3,86 @@
 # @Project: CyberConquest/CyberConquest2024/demo
 # @Author: Goofables
 # @Created: 2024-04-17
+import datetime
+import logging
 import math
-import time
+import random
 
 import adafruit_is31fl3741
 import board
 from adafruit_is31fl3741.adafruit_rgbmatrixqt import Adafruit_RGBMatrixQT
 
+logger = logging.getLogger(__name__)
+
 
 def color_val(r: int, g: int, b: int) -> int:
-    return (r << 16) | (g << 8) | b
+    # return (r << 16) | (g << 8) | b
+    if r == 0 and g == 0 and b == 0:
+        return 0
+    return random.randint(0, 256 * 256 * 256 - 1)
 
 
-print("Initializing board")
-i2c = board.I2C()  # uses board.SCL and board.SDA
-is31 = Adafruit_RGBMatrixQT(i2c, allocate=adafruit_is31fl3741.PREFER_BUFFER)
-is31.set_led_scaling(0xFF)
-is31.global_current = 0xFF
-is31.set_led_scaling(20)
-print("Global current is: ", is31.global_current)
-is31.enable = True
+class ClockTower:
+    def __init__(self):
+        logger.info("Initializing board")
+        # Initialize the rgb matrix
+        self.is31 = Adafruit_RGBMatrixQT(board.I2C(), allocate=adafruit_is31fl3741.PREFER_BUFFER)
+        # Multiplier for each pixel value
+        self.is31.set_led_scaling(0xFF)
+        # Total current allowance
+        self.is31.global_current = 0x01
+        # Enable the matrix
+        self.is31.enable = True
+
+        self.CENTER_X = 6.5
+        self.CENTER_Y = 4.5
+
+    def xy_from_ar(self, a: float | int, r: float | int = 100, a_max: int = 100, r_max: int = 100) -> tuple[int, int]:
+        """
+        Calculate the x and y coordinates of an angle and radius projected from the center of the board.
+
+        @param a: Angle from 0 to `a_max`
+        @param r: Radius from 0 to `r_max`
+        @param a_max: Max for angle
+        @param r_max: Max for radius
+        @return: A tuple of x and y coordinates
+        """
+        a = float(a) * math.pi * 2 / float(a_max)
+        r = float(r) * 4.3 / float(r_max)
+        return int(self.CENTER_X + math.sin(a) * r), int(self.CENTER_Y + -math.cos(a) * r)
+
+    def tick(self) -> None:
+        """Update the display"""
+        self.is31.fill(color_val(0, 0, 0))
+        for i in range(100):
+            self.is31.pixel(
+                *self.xy_from_ar(i),
+                color_val(255, 100, 0),
+            )
+        now = datetime.datetime.now()
+        for i in range(0, 100, 5):
+            self.is31.pixel(
+                *self.xy_from_ar(now.hour % 12, i, a_max=12),
+                color_val(255, 0, 0),
+            )
+            self.is31.pixel(
+                *self.xy_from_ar(now.minute, i, a_max=60),
+                color_val(0, 255, 0),
+            )
+            self.is31.pixel(
+                *self.xy_from_ar(now.second, i, a_max=60),
+                color_val(0, 0, 255),
+            )
+
+        self.is31.show()
+
+    def clear(self) -> None:
+        """Clear the display"""
+        self.is31.fill(color_val(0, 0, 0))
+        self.is31.show()
 
 
-CENTER_Y = 4
-CENTER_X = 6
-
-# for y in range(9):
-#     for x in range(13):
-#         is31.pixel(x, y, color_val(20, 0, 0))
-#         is31.show()
-
-print("Playing animation:")
-
-for i in range(360):
-    r = i * math.pi / 180.0
-    is31.pixel(int(CENTER_X + math.cos(r) * 4), int(CENTER_Y + math.sin(i) * 4), color_val(i, 255, 0))
-    is31.show()
-
-
-time.sleep(10)
-
-# def
-#
-# while True:
-#
+if __name__ == "__main__":
+    tower = ClockTower()
+    while True:
+        tower.tick()
